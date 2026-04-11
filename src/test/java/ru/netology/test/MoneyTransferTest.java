4deeb13bb90c8adbc34ca373dbd882290cb8f695
nnekit10 <nnekit10@yandex.ru>
2026-04-11 12:22:31 +0300

    first commit

---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,38 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPageV1;
+import ru.netology.page.LoginPageV2;
+import ru.netology.page.LoginPageV3;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        var loginPage = open("http://localhost:9999", LoginPageV1.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        var loginPage = open("http://localhost:9999", LoginPageV2.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV3() {
+        var loginPage = open("http://localhost:9999", LoginPageV3.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+}
